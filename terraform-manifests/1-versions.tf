bd46965b2bfdfd67827b977d8bd51e6bd5dfa2e1
ch798543 <111711851+ch798543@users.noreply.github.com>
2022-08-29 12:06:05 -0700

    Update 1-versions.tf

--- terraform-manifests/1-versions.tf
+++ terraform-manifests/1-versions.tf
@@ -1,24 +1,24 @@
+# Terraform Block
 terraform {
-  required_version = ">=1.0.0"
+  required_version = ">= 1.0.0"
   required_providers {
     azurerm = {
-        source = "hashicorp/azurerm"
-        version = ">=2.0.0"
+      source = "hashicorp/azurerm"
+      version = ">= 2.0" 
     }
     random = {
-        source = "hashicorp/random"
-        version =">=2.0.0" 
-        }
+      source = "hashicorp/random"
+      version = ">= 3.0"
     }
-
-  }
-
-
-provider "azurerm" {
-  features {
-    
   }
 }
+
+# Provider Block
+provider "azurerm" {
+ features {}          
+}
+
+
 
 resource "random_string" "myrandom" {
   length = 8
